f8a75938477021117037960ba8e8df6abf769665
umamit <shot.ann09@gmail.com>
2026-06-09 20:06:12 +0900

    fix(build): configure webpack build and Vercel Node/Next settings

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // Disable Turbopack warning and use Webpack for build
+  turbopack: {},
+  experimental: {
+    cpus: 1,
+    optimizePackageImports: ['@supabase/supabase-js'],
+  },
+
+  outputFileTracingRoot: __dirname,
+
+  // Optimize Webpack build
+  webpack: (config, { isServer }) => {
+    config.optimization.minimize = true;
+
+    // Reduce memory during build
+    if (!isServer) {
+      config.optimization.splitChunks.chunks = 'all';
+      config.optimization.runtimeChunk = 'single';
+    }
+
+    return config;
+  },
+
+  // Image optimization
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+};
+
+module.exports = nextConfig;
