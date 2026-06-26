e44835dc2c8b410b45537e5c29aa09f373190501
umamit <shot.ann09@gmail.com>
2026-06-26 14:19:12 +0900

    fix: update import path for getMonthName from ../../../utils to ../../utils

--- src/app/admin/finance/components/FinanceAnalytics.jsx
+++ src/app/admin/finance/components/FinanceAnalytics.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { getMonthName } from "../../../utils";
+import { getMonthName } from "../../utils";
 
 export default function FinanceAnalytics({
   students,
