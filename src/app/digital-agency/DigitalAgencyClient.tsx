46ca821fc97415ef186b90a90d972b921f31ba36
umamit <shot.ann09@gmail.com>
2026-08-01 14:50:10 +0900

    fix(logo): rename logo file to ibra-digital-logo-v2.png to force bypass browser and edge caching

--- src/app/digital-agency/DigitalAgencyClient.tsx
+++ src/app/digital-agency/DigitalAgencyClient.tsx
@@ -131,7 +131,7 @@ Mohon hubungi saya kembali. Terima kasih!`;
         <div className={styles.navContainer}>
           <a href="#" className={styles.logo}>
             <img 
-              src="/assets/ide-logo.png" 
+              src="/assets/ibra-digital-logo-v2.png" 
               alt="Ibra Digital Engineering Logo" 
               width={147} 
               height={80} 
@@ -663,7 +663,7 @@ Mohon hubungi saya kembali. Terima kasih!`;
       <footer className={styles.footer}>
         <div style={{ marginBottom: "16px", display: "flex", justifyContent: "center" }}>
           <img 
-            src="/assets/ide-logo.png" 
+            src="/assets/ibra-digital-logo-v2.png" 
             alt="Ibra Digital Engineering Logo" 
             width={156} 
             height={85} 
